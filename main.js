e21d9e47b61d8a240311cc04ee9dc074703d0a29
jimdog6125 <24468320+jimdog6125@users.noreply.github.com>
2018-06-08 12:14:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var a = 0;
 var b = 0;
+var c = 0;
 var save = {
     a: a,
     b: b,
+    c: c
 }
 
 function save(){
@@ -15,6 +17,8 @@ if (typeof savegame.a !== "undefined") cookies = savegame.a;
     document.getElementById("a").innerHTML = a;
 if (typeof savegame.b !== "undefined") cookies = savegame.b;
     document.getElementById("b").innerHTML = b;
+if (typeof savegame.c !== "undefined") cookies = savegame.c;
+    document.getElementById("c").innerHTML = c;
 }
 load;
 
@@ -32,9 +36,22 @@ function buyB(){
         document.getElementById('b').innerHTML = b;  
         document.getElementById('a').innerHTML = a;  
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,b));       
+    var nextCost = Math.floor(10 * Math.pow(1.3,c));       
     document.getElementById('bCost').innerHTML = nextCost;  
 };
+
+function buyC(){
+    var cCost = Math.floor(10 * Math.pow(1.1,c));     
+    if(b >= cCost){                                  
+        c = c + 1;                                   
+    	b = b - cCost;                         
+        document.getElementById('c').innerHTML = c;  
+        document.getElementById('b').innerHTML = b;  
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.3,c));       
+    document.getElementById('cCost').innerHTML = nextCost;  
+};
+
 
 
 window.setInterval(function(){
